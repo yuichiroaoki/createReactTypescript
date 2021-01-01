7a5e027d826f1d2e6c69e8ae611cc28809ad2828
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-01-01 11:50:22 +0900

    add material ui

---
+++ src/theme.tsx
@@ -0,0 +1,24 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+    palette: {
+      type: "light",
+      primary: {
+        light: '#63ccff',
+        main: '#009be5',
+        dark: '#006db3',
+      },
+      secondary: {
+        main: '#19857b',
+      },
+      error: {
+        main: red.A400,
+      },
+      background: {
+        default: '#fff',
+      },
+    },
+  });
+  
+  export default theme;
