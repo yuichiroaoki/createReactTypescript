0e68863603be095db1427ccca1860b94054d2cb2
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-01-02 15:20:46 +0900

    add timer

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -3,35 +3,44 @@ import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import { Counter } from "./Counter";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-    },
-    title: {
-      flexGrow: 1,
-    },
-    content: {
-      flexGrow: 1,
-      height: '100vh', 
-      overflow: 'auto',
-    },
-    paper: {
-      padding: theme.spacing(2),
-      display: 'flex',
-      overflow: 'auto',
-      flexDirection: 'column',
-    }
+  root: {
+    display: 'flex',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  }
 }));
 
 export default function Contents() {
-    const classes = useStyles();
-  
-    return (
-        <Container>
-            <Typography variant="h1">
-                Hello
+  const classes = useStyles();
+
+  return (
+    <Container>
+      <Typography variant="h1">
+        Hello
             </Typography>
-        </Container>
-    );
-  }
+      <Counter>
+        {({ count, setCount }) => (
+          <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+        )}
+      </Counter>
+    </Container>
+  );
+}
--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,10 +1,42 @@
 // type rh
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
-interface ExampleProps {
+// interface ExampleProps {
 
+// }
+
+// export const Example: React.FC<ExampleProps> = ({}) => {
+//         return (
+//                 <div>{}</div>
+//         );
+// }
+
+interface Person {
+  firstName: string;
+  lastName: string;
 }
 
-export const Example: React.FC<ExampleProps> = ({}) => {
-        return ();
+interface Props {
+  text: string;
+  ok?: boolean;
+  i?: number;
+  fn?: (bob: string) => string;
+  person: Person;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
+
+interface TextNode {
+  text: string;
+}
+
+export const TextField: React.FC<Props> = ({ handleChange }) => {
+  const [count, setCount] = useState<TextNode>({ text: "hello" });
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div ref={divRef}>
+      <input ref={inputRef} onChange={handleChange} />
+    </div>
+  );
+};
