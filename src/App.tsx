a27f99be2631c701d2e7d14930dda6bcf7dac8f2
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-01-02 15:26:06 +0900

    delete logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 import Home from './components/Home';
