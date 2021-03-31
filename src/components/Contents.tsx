63f71174ed079b990d4c0be4d0ec72dfb817b8b0
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-03-31 12:22:23 +0900

    delete package-lock

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -1,36 +1,13 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import { Counter } from "./Counter";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  }
-}));
+import {TextField} from "./Example";
 
 export default function Contents() {
-  const classes = useStyles();
-
   return (
     <Container>
-      <Typography variant="h1">
+      <Typography>
         Hello
       </Typography>
       <Counter>
@@ -41,6 +18,7 @@ export default function Contents() {
           </div>
         )}
       </Counter>
+      <TextField />
     </Container>
   );
 }
