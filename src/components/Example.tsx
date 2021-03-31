b60efaeaccafddb509f9ae082b0f8570e6f74e4e
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-03-31 12:32:55 +0900

    fix bugs :bug:

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,5 +1,5 @@
 // type rh
-import React, { useState, useRef } from 'react'
+import React, { useRef } from 'react'
 
 // interface ExampleProps {
 
@@ -17,20 +17,16 @@ interface Person {
 }
 
 interface Props {
-  text: string;
+  text?: string;
   ok?: boolean;
   i?: number;
   fn?: (bob: string) => string;
-  person: Person;
+  person?: Person;
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-interface TextNode {
-  text: string;
-}
 
 export const TextField: React.FC<Props> = ({ handleChange }) => {
-  const [count, setCount] = useState<TextNode>({ text: "hello" });
   const inputRef = useRef<HTMLInputElement>(null);
   const divRef = useRef<HTMLDivElement>(null);
 
