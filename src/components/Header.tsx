7d014b563f188e1b881ac5d5e6e112424c773b0c
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-03-31 13:51:03 +0900

    fix bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import Navigator from './Navigator';
